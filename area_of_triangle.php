840f780a55c60424b75935873c6819665d02494d
Hrishikesh Kolte <85955896+hrishi3007@users.noreply.github.com>
2024-06-02 22:52:48 +0530

    without o/p

---
+++ area_of_triangle.php
@@ -0,0 +1,10 @@
+<!-- Area of Triangle = (base * height) / 2 = Area  
+-->
+
+
+
+<?php  
+ $base = 10;  
+ $height = 15;  
+ echo "area with base $base and height $height= " . ($base * $height) / 2;  
+ ?>  
